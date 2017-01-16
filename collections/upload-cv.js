d90636e35f33cd4e6bff930a6f65ad6686e07f91
Joël ALAUX <joel.alaux@gmail.com>
2017-01-16 01:30:41 +0100

    without 3rd party storage

--- collections/upload-cv.js
+++ collections/upload-cv.js
@@ -53,54 +53,54 @@ export const CV_files = new FilesCollection({
         if (file.size <= 10485760 && /pdf/i.test(file.extension)) return true;
         return 'only pdf file < 10Mb';
     },
-    onAfterUpload(file) {
-        // console.log(file);
-        // Move file to GridFS
-        Object.keys(file.versions).forEach(versionName => {
-            const metadata = {
-                versionName,
-                fileId: file._id,
-                storedAt: new Date(),
-            }; // Optional
-            const writeStream = gfs.createWriteStream({
-                filename: file.name,
-                metadata
-            });
-
-            fs.createReadStream(file.versions[versionName].path).pipe(writeStream);
-
-            writeStream.on('close', Meteor.bindEnvironment(file => {
-                const property = `versions.${versionName}.meta.gridFsFileId`;
-                // If we store the ObjectID itself, Meteor (EJSON?) seems to convert it to a
-                // LocalCollection.ObjectID, which GFS doesn't understand.
-                this.collection.update(file._id.toString(), {
-                    $set: {
-                        [property]: file._id.toString()
-                    }
-                });
-                // console.log("UNLINK : ");
-                // console.log(this.collection.find(file._id));
-                this.unlink(this.collection.find(file._id), versionName); // Unlink file by version from FS
-            }));
-        });
-    },
-    interceptDownload(http, file, versionName) {
-        // Serve file from GridFS
-        // console.log("interceptDownload");
-        //
-        // console.log(file);
-        const _id = (file.versions[versionName].meta || {}).gridFsFileId;
-        if (_id) {
-            const readStream = gfs.createReadStream({
-                _id
-            });
-            readStream.on('error', err => {
-                throw err;
-            });
-            readStream.pipe(http.response);
-        }
-        return Boolean(_id); // Serve file from either GridFS or FS if it wasn't uploaded yet
-    },
+    // onAfterUpload(file) {
+    //     // console.log(file);
+    //     // Move file to GridFS
+    //     Object.keys(file.versions).forEach(versionName => {
+    //         const metadata = {
+    //             versionName,
+    //             fileId: file._id,
+    //             storedAt: new Date(),
+    //         }; // Optional
+    //         const writeStream = gfs.createWriteStream({
+    //             filename: file.name,
+    //             metadata
+    //         });
+    //
+    //         fs.createReadStream(file.versions[versionName].path).pipe(writeStream);
+    //
+    //         writeStream.on('close', Meteor.bindEnvironment(file => {
+    //             const property = `versions.${versionName}.meta.gridFsFileId`;
+    //             // If we store the ObjectID itself, Meteor (EJSON?) seems to convert it to a
+    //             // LocalCollection.ObjectID, which GFS doesn't understand.
+    //             this.collection.update(file._id.toString(), {
+    //                 $set: {
+    //                     [property]: file._id.toString()
+    //                 }
+    //             });
+    //             // console.log("UNLINK : ");
+    //             // console.log(this.collection.find(file._id));
+    //             this.unlink(this.collection.find(file._id), versionName); // Unlink file by version from FS
+    //         }));
+    //     });
+    // },
+    // interceptDownload(http, file, versionName) {
+    //     // Serve file from GridFS
+    //     // console.log("interceptDownload");
+    //     //
+    //     // console.log(file);
+    //     const _id = (file.versions[versionName].meta || {}).gridFsFileId;
+    //     if (_id) {
+    //         const readStream = gfs.createReadStream({
+    //             _id
+    //         });
+    //         readStream.on('error', err => {
+    //             throw err;
+    //         });
+    //         readStream.pipe(http.response);
+    //     }
+    //     return Boolean(_id); // Serve file from either GridFS or FS if it wasn't uploaded yet
+    // },
     onAfterRemove(CV_files) {
         // Remove corresponding file from GridFS
         // console.log("onAfterRemove");
